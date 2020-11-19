d39bc96fcdfcd52bfb858719a3243daed2242006
Mamadou WONE <mamadouwone12345@gmail.com>
2020-11-19 14:15:39 +0000

    19/11/2020

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Test extends JFrame{
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Boss");
+        frame.setSize(420,420);
+        frame.setVisible(true);
+    }
+}
